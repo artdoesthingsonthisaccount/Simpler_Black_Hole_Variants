7f83ea22ee6242d868ebf226555fc3780130aad7
Art Muley <asmuley2@illinois.edu>
2025-09-02 14:21:15 -0500

    added export x11 command as a comment and some important notes on how all the cmake stuff works.

--- src/2d_bh_sim/main.cpp
+++ src/2d_bh_sim/main.cpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <iostream>
 
+//export line:
+//export LIBDECOR_PLUGIN_DIR=/usr/lib/x86_64-linux-gnu/libdecor/plugins-1
+//contains libdecor-gtk.so
+
+
 int main(){
     std::cout << "hello blackhole\n";
     return 0;
